f2ab85feb51c35eeb4fdbf95818e5bf1ccb42941
Tammy Ho <momo09041027@gmail.com>
2025-04-15 22:18:52 +0800

    1.新增商品列表渲染(json)2.新增分頁並綁定按鈕點擊事件3.微調商品列表css

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # jQuery E-commerce Website
 
-這原本是一個jQuery製作的電商網站，用來模擬線上購物的商品瀏覽、購物車互動功能。
+製作實際電商網站會有的幾個常見功能，大部分皆Vanilla Javascript為主，API部分會以jQ為主
 
 ---
 
 ## 預期功能介紹
 
-- 商品列表展示（jQuery 配合JSON檔案 動態渲染）
-- 商品加入購物車（可即時增加數量）
-- 移除商品、計算總金額
-- 購物車 icon 數量更新
-- 會員註冊與登入(LocalStorage作為測試環境)
+- 會員註冊、登入(以localStorage作為資料儲存區域) (2025/04/14新增)
+- 商品列表展示（jQuery 配合JSON檔案 動態渲染）(2025/04/15新增)
+  - 延伸練習 1.分頁 2.分類 3.搜尋
+- 商品加入購物車，存進會員資料庫
+- 購物車細節(商品數量增減、金額計算、icon更新)
+- 結帳頁面(模擬真實結帳，保存歷史訂單)
 
 ---
 
 ## 使用技術
 
-- HTML5 / CSS3
+- HTML5 / CSS3 / SASS(SCSS)
 - JavaScript / jQuery
 - jQuery Ajax（若有模擬串接 API）
 - localStorage（模擬儲存購物車資料）
@@ -41,3 +42,9 @@
 1. 將此專案 clone 至本機端：
    ```bash
    git clone https://github.com/tamytammy/ecommerce-website.git
+
+---
+
+##作者資訊
+Email: momo09041027@gmail.com
+我是小德!
--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,70 @@ function getItem(key){
 function removeItem(key){
     localStorage.removeItem(key)
 }
+
+$(document).ready(function(){
+    $.ajax({
+        url: './assets/data/products.json',
+        dataType: 'json',
+        type: 'GET',
+        success:function(itemData){
+            pageDivider(itemData)
+        },
+        erroo:function(itemData){
+            alert('Get JSON檔案失敗!')
+        }
+    })
+
+    function renderProducts(itemData){
+        $('.products').empty();
+        itemData.forEach(item=>{
+                let name = item.name
+                let price = item.price
+                let img = item.img
+                if(!img){
+                    img = './assets/img/notfount.png'
+                }
+                let html = ''
+                html = "<div class='product-box'><div class='product-box__img'><img src='"+ img +"' alt=''></div><div class='product-box__text'><p class='product-box__text-name'>"+ name +"</p><div class='product-box__text-detail'><span class='price'>$"+ price +"</span></div></div></div>"
+                $('.products').append(html);
+            })
+    }
+    function pageDivider(itemData){
+        $('.pageSelector').empty();
+
+        let currentPage = 1 //當前頁面
+        const itemPerPage = 12 //每頁顯示數量
+        const pageTotalAmount = Math.ceil(itemData.length / itemPerPage) //換算總頁數
+
+
+        //生成按鈕
+        for(let i = 1; i <= pageTotalAmount ; i++){
+            $('.pageSelector').append(`
+                    <button type="button" class="page-btn" data-page="${i}"><span>${i}</span></button>
+            `)
+        }
+        
+        //先渲染預設第一頁
+        const initProducts = itemData.slice(0, itemPerPage)
+        renderProducts(initProducts)
+
+        //綁定頁面點擊按鈕
+        $('.pageSelector').on('click','.page-btn',function(){
+            
+            currentPage = $(this).data('page') //根據設定data賦予currentPage值
+            let start = (currentPage - 1) * itemPerPage 
+            let end = start + itemPerPage
+            let productToShow = itemData.slice(start, end) //計算要渲染的商品
+
+            renderProducts(productToShow)
+
+            //樣式
+
+        })
+        
+
+        
+
+    }
+    
+})
