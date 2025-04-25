ecbec58e32999794de1571c51e3e7264475363f7
Tammy Ho <momo09041027@gmail.com>
2025-04-25 11:51:36 +0800

    feat: 結帳按鈕儲存資料到localStorage, SessionStorage, 步驟三渲染購物車內容與個人付款資訊 style: 結帳頁面步驟三排版與樣式

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,16 +1,81 @@
-function setItem(key, value){
+function setItemTep(key, value){
     sessionStorage.setItem(key, JSON.stringify(value))
 }
 
-function getItem(key){
+function getItemTep(key){
     return JSON.parse(sessionStorage.getItem(key))
 }
 
-function nextStep(){
+function saveInfoTemp(){
     //抓html元素的值、存進sessionStorage
+    const checkUsername = document.getElementById('checkout-name').value
+    const checkPhone = document.getElementById('checkout-phone').value
+    const checkMail = document.getElementById('checkout-mail').value
+    const checkAdr = document.getElementById('checkout-address').value
+    const checkPay = document.getElementById('checkout-payment').value
 
+    const checkInfo = {
+        checkUsername,
+        checkPhone,
+        checkMail,
+        checkAdr,
+        checkPay
+    }
+    const users = getItem('users') || []
+    const user = users.find(u => u.username === getItem('loggedInStatus'))
+    if(user.orders){
+        //if there is already have the checkInfo in the orders, just update it with the new one
+        user.orders[0] = checkInfo        
+    }else{
+        user.orders.push(checkInfo)
+    }
+    setItem('users', users)
+    setItemTep('users', [user])
 }
+function renderCheckout(){
+    const loginUser = getItem('loggedInStatus')
+    let users = getItem('users') || []
+    const user = users.find(u => u.username === loginUser)
 
+    cartData = user.cart || []
+    $('.order-cart').empty()
+    cartData.forEach(data => {
+        $('.order-cart').append(`
+            <div class="order__item">
+                <p>${data.name}</p>
+                    <div class="order__item-img">
+                     <img src="${data.image}" alt="${data.name}">
+                     </div>
+                     <div class="order__item-amount">
+                      <span class="itemNum">${data.quantity}</span>
+                    </div>
+                    <div class="order__item-price">
+                       $
+                     <span class="itemPrice">${data.price}</span>
+                    </div>
+            </div>
+        `)     
+    })
+}
+function renderCheckInfo(){
+    let users = getItemTep('users') || []
+    const user = users.find(u => u.username === getItem('loggedInStatus'))
+    const checkInfo = user.orders
+
+    $('.personal__info').empty()
+
+    checkInfo.forEach(info=>{
+        $('.personal__info').append(`
+            <div class="personal__info-text">
+                <p>姓名：<span>${info.checkUsername}</span></p>
+                <p>電話：<span>${info.checkPhone}</span></p>
+                <p>Email：<span>${info.checkMail}</span></p>
+                <p>收件地址：<span>${info.checkAdr}</span></p>
+                <p>付款方式：<span>${info.checkPay}</span></p>
+            </div>
+        `)
+    })
+}
 //點擊nextBtn前往下一步驟，下一個div data-step="2"
 $(document).on('click', '.nextBtn', function(){
     const num = $(this).data('step')
@@ -31,7 +96,11 @@ $(document).on('click', '.nextBtn', function(){
                 b.nextElementSibling.classList.add('active')
                 b.classList.remove('active')
             }
-        })
+    })
+
+    saveInfoTemp();
+    renderCheckout();
+    renderCheckInfo();
 
     
 })
